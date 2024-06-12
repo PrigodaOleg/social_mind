0532da1feb581a0e070dbbf54434465ca78cda5d
PrigodaOleg <prigodaoleg@gmail.com>
2024-06-12 20:11:32 +0300

    DEbug

--- domains/client/lib/main.dart
+++ domains/client/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 
 Future<void> main() async {
   // await Firebase.initializeApp(
