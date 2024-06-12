7a7f5967c895e3f11561a3c41382d198c7caa422
PrigodaOleg <prigodaoleg@gmail.com>
2024-06-12 20:02:47 +0300

    Temporarly disable firebase connection

--- domains/client/lib/main.dart
+++ domains/client/lib/main.dart
@@ -5,9 +5,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const App());
 }
