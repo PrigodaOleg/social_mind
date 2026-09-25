cad00f1174bc5560318c8a9cff32dde100b0beb8
ArchibaldovRPtech <archiprigoda@gmail.com>
2026-09-25 18:20:11 +0300

    fixing after faulty pull

--- domains/client/lib/repository/models/user.dart
+++ domains/client/lib/repository/models/user.dart
@@ -1,6 +1,5 @@
 part of 'models.dart';
 
-
 @HiveType(typeId: 1)
 // ignore: must_be_immutable
 class User extends Model {
@@ -20,25 +19,18 @@ class User extends Model {
         lastName = lastName ?? '',
         description = description ?? '';
 
-(??)  User.fromJson(super.json)
-(??)      : name = json['name'] as String,
-(??)    domainsIds = List<String>.from(json['domainsIds'] is List ? json['domainsIds'] : json['domainsIds']?.keys ?? []),
-(??)        registryId = (json['registryId'] ?? []) as String,
-(??)        avatarUrl = json['avatarUrl'] as String,
-(??)        firstName = json['firstName'] as String,
-(??)        lastName = json['lastName'] as String,
-(??)        description = json['description'] as String,
-(??)        super.fromJson();
-(??)
-(??)<<<<<<< HEAD
-(??)=======
-(??)  User.fromJson(super.json) :
-(??)    name = json['name'] as String,
-(??)    domainsIds = List<String>.from(json['domainsIds'] is List ? json['domainsIds'] : json['domainsIds']?.keys ?? []),
-(??)    registryId = (json['registryId'] ?? []) as String,
-(??)    super.fromJson();
-(??)    
-(??)>>>>>>> origin/main
+  User.fromJson(super.json)
+      : name = json['name'] as String,
+        domainsIds = List<String>.from(json['domainsIds'] is List
+            ? json['domainsIds']
+            : json['domainsIds']?.keys ?? []),
+        registryId = (json['registryId'] ?? []) as String,
+        avatarUrl = json['avatarUrl'] as String,
+        firstName = json['firstName'] as String,
+        lastName = json['lastName'] as String,
+        description = json['description'] as String,
+        super.fromJson();
+
   @override
   // ignore: overridden_fields
   final String type = "User";
@@ -51,18 +43,20 @@ class User extends Model {
 
   String registryId;
 
+  String avatarUrl;
+  String firstName;
+  String lastName;
+  String description;
+
   Map<String, dynamic> settings = {
     'remote_storages': <String, dynamic>{
-      'FirebaseRealtimeDatabase': <String, dynamic>{
-        'instance': 'closers-cd24f'
-      }}
+      'FirebaseRealtimeDatabase': <String, dynamic>{'instance': 'closers-cd24f'}
+    }
   };
 
   Map<String, dynamic> secrets = {
     'remote_storages': <String, dynamic>{
-      'FirebaseRealtimeDatabase': <String, dynamic>{
-        'hashed_password': null
-      }
+      'FirebaseRealtimeDatabase': <String, dynamic>{'hashed_password': null}
     }
   };
 
@@ -84,10 +78,7 @@ class User extends Model {
     });
 
   @override
-  void link({
-    Model? to,
-    Model? from
-  }) {
+  void link({Model? to, Model? from}) {
     switch (to) {
       case Domain():
         domainsIds.add(to.id);
@@ -98,10 +89,7 @@ class User extends Model {
   }
 
   @override
-  void unlink({
-    Model? to,
-    Model? from
-  }) {
+  void unlink({Model? to, Model? from}) {
     switch (from) {
       case Domain():
         domainsIds.remove(from.id);
@@ -133,36 +121,40 @@ class User extends Model {
     );
   }
 
-  List<Map<String,dynamic>> propsList(){
+  List<Map<String, dynamic>> propsList() {
     return [
-      {'propId':'id','value':id,'isEditable':false},
-      {'propId':'name','value':name,'isEditable':true},
-      {'propId':'avatarUrl','value':avatarUrl,'isEditable':true},
-      {'propId':'firstName','value':firstName,'isEditable':true},
-      {'propId':'lastName','value':lastName,'isEditable':true},
-      {'propId':'description','value':description,'isEditable':true},
+      {'propId': 'id', 'value': id, 'isEditable': false},
+      {'propId': 'name', 'value': name, 'isEditable': true},
+      {'propId': 'avatarUrl', 'value': avatarUrl, 'isEditable': true},
+      {'propId': 'firstName', 'value': firstName, 'isEditable': true},
+      {'propId': 'lastName', 'value': lastName, 'isEditable': true},
+      {'propId': 'description', 'value': description, 'isEditable': true},
     ];
   }
 
   //todo
-  User copyWithList(List<Map<String,dynamic>> list){
-
+  User copyWithList(List<Map<String, dynamic>> list) {
     String? name;
     String? avatarUrl;
     String? firstName;
     String? lastName;
     String? description;
-   
-   for(final record in list){
-     final propId = record['propId'];
-     switch(propId){
-     case 'name': name = record['value'];
-     case 'avatarUrl': avatarUrl = record['value'];
-     case 'firstName': firstName = record['value'];
-     case 'lastName': lastName = record['value'];
-     case 'description': description = record['value'];
-     }
-   }
+
+    for (final record in list) {
+      final propId = record['propId'];
+      switch (propId) {
+        case 'name':
+          name = record['value'];
+        case 'avatarUrl':
+          avatarUrl = record['value'];
+        case 'firstName':
+          firstName = record['value'];
+        case 'lastName':
+          lastName = record['value'];
+        case 'description':
+          description = record['value'];
+      }
+    }
     return copyWith(
       name: name,
       avatarUrl: avatarUrl,
@@ -170,8 +162,6 @@ class User extends Model {
       lastName: lastName,
       description: description,
     );
-
-
   }
 
   @override
